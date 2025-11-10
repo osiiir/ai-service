9efdcff909352eb121d0b041716b070f3d350179
osir <osir@xxx.com>
2025-11-10 22:30:15 +0800

    Initial commit

---
+++ src/main/java/com/osir/springai/OsirSpringaiApplication.java
@@ -0,0 +1,13 @@
+package com.osir.springai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsirSpringaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsirSpringaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osir/springai/OsirSpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osir.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsirSpringaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
